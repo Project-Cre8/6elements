e474063318ceffcaaf3d25e172c915d28fad35b9
古田朋宏 <tomohiro@MacBook-Pro.local>
2020-11-05 16:56:38 -0500

    add github actions

--- truffle-config.js
+++ truffle-config.js
@@ -1,12 +1,10 @@
 const path = require('path');
 const HDWalletProvider = require('@truffle/hdwallet-provider');
-const infuraKey = 'd5c27acd7a21458697af3bab3723cd14';
 
-const mnemonic = 'inspire vital pill wood help avocado enter flip ostrich wife sadness giggle';
+const infuraKey = '';
+const mnemonic = '';
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, 'client/src/contracts'),
   networks: {
     develop: {
@@ -17,12 +15,15 @@ module.exports = {
       port: 8545,
       network_id: '*',
     },
+    test: {
+      host: '127.0.0.1',
+      port: 7545,
+      network_id: '*',
+    },
     kovan: {
       provider: function () {
         return new HDWalletProvider(mnemonic, `https://kovan.infura.io/v3/${infuraKey}`);
       },
-      
-      
       network_id: 42,
     },
     ropsten: {
@@ -32,7 +33,7 @@ module.exports = {
       gas: 5000000,
       gasPrice: 25000000000,
       network_id: 3,
-    }
+    },
   },
   compilers: {
     solc: {
